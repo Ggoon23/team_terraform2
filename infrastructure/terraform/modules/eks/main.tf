2e513d96ba742c39216a6e3491a7507fd2fc8c06
김태곤 <xogoon@naver.com>
2025-07-24 09:35:55 +0900

    configmap제거

--- infrastructure/terraform/modules/eks/main.tf
+++ infrastructure/terraform/modules/eks/main.tf
@@ -782,25 +782,3 @@ locals {
     }
   ], local.admin_roles_map, var.map_roles)
 }
-
-# 개선된 Kubernetes ConfigMap
-resource "kubernetes_config_map_v1" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles = yamlencode(local.all_role_mappings)
-    mapUsers = yamlencode(local.all_user_mappings)
-  }
-
-  depends_on = [aws_eks_cluster.main, aws_eks_node_group.main]
-  
-  lifecycle {
-    ignore_changes = [metadata[0].annotations]
-  }
-}
-
-# EKS 클러스터가 생성될 때 AWS가 자동으로 aws-auth ConfigMap을 생성하는데, Terraform 코드에서도 이를 생성하려고 해서 충돌이 발생
-# terraform import module.eks.kubernetes_config_map_v1.aws_auth kube-system/aws-auth
